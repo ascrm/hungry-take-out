8c9fd65732976513831c826b872d011dea2e47aa
ascrm <2339621373@qq.com>
2024-04-28 20:36:49 +0800

    上传了前端部分。暂时只完成了注册登功能和菜品模块的页面。

--- src/main/java/com/hungry/controller/DishController.java
+++ src/main/java/com/hungry/controller/DishController.java
@@ -2,22 +2,41 @@ package com.hungry.controller;
 
 import com.hungry.pojo.PageInfo;
 import com.hungry.pojo.Result;
+import com.hungry.pojo.entity.Dish;
 import com.hungry.service.impl.DishServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/page")
+@RequestMapping("/dish")
 @RequiredArgsConstructor
 @Slf4j
 public class DishController {
     private final DishServiceImpl dishService;
-    @GetMapping
-    public Result<PageInfo> page(@RequestParam int pageNum, @RequestParam int pageSize) {
+
+    /*TODO 问题
+    1. num为 1 的时候是从第二条数据开始查的
+    2. 返回的total总记录数，是表里面的所有数据，而不是返回的记录数的总数居
+    3. pageInfo只需要 total 和 data 两个属性
+     */
+    @GetMapping("/page")
+    public Result<PageInfo<Dish>> page(@RequestParam int pageNum, @RequestParam int pageSize) {
         return Result.success(dishService.MyPageList(pageNum,pageSize));
+    }
+
+    @PostMapping("/edit")
+    public Result<String> editDish(@RequestBody Dish dish){
+        return null;
+    }
+
+    @PutMapping("/add")
+    public Result<String> addDish(@RequestBody Dish dish){
+        return null;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Result<String> deleteDish(@PathVariable Integer id){
+        return null;
     }
 }
